a4954939ff19ce1d7e29078395cf20f518b1ad3c
MSI\antar <alaktalapatra@gmail.com>
2026-04-24 16:02:16 +0530

    Initial commit - SmartFleet project

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const vehicleRoutes = require("./routes/vehicleRoutes");
 const { startSimulation, getLiveLocation } = require("./utils/simulator");
 
+const client = require('prom-client');
+client.collectDefaultMetrics();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,6 +17,11 @@ app.use("/api", vehicleRoutes);
 // Direct API route for live location (alternative to controller)
 app.get("/api/live-location", (req, res) => {
   res.json(getLiveLocation());
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 // Start vehicle simulation when server starts
